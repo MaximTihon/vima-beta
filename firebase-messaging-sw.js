e9510c68b139cb68671d377f91222865c99cf25d
max-Lenova <max21250@mail.ru>
2019-06-09 23:51:41 +0300

    vcvcvcvczf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,3 +12,23 @@ Notification.requestPermission().then(function(permission) {
     console.log('Unable to get permission to notify.');
   }
 });
+
+
+// Get Instance ID token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken().then(function(currentToken) {
+  if (currentToken) {
+    sendTokenToServer(currentToken);
+    updateUIForPushEnabled(currentToken);
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+    // Show permission UI.
+    updateUIForPushPermissionRequired();
+    setTokenSentToServer(false);
+  }
+}).catch(function(err) {
+  console.log('An error occurred while retrieving token. ', err);
+  showToken('Error retrieving Instance ID token. ', err);
+  setTokenSentToServer(false);
+});
