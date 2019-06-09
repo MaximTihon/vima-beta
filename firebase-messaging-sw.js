bcf5585580ab8e34c374f259a700eeca4b637f41
max-Lenova <max21250@mail.ru>
2019-06-09 20:43:15 +0300

    sss

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,4 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
 
 firebase.initializeApp({
     messagingSenderId: 309655316986
