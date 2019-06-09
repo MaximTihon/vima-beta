693654cff8053265365a3b6927047874980a96a2
max-Lenova <max21250@mail.ru>
2019-06-09 21:29:30 +0300

    dasdasd

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,4 +6,45 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-console.log(messaging)
+// браузер поддерживает уведомления
+// вообще, эту проверку должна делать библиотека Firebase, но она этого не делает
+if ('Notification' in window) {
+    var messaging = firebase.messaging();
+
+    // пользователь уже разрешил получение уведомлений
+    // подписываем на уведомления если ещё не подписали
+    if (Notification.permission === 'granted') {
+        subscribe();
+    }
+
+    // по клику, запрашиваем у пользователя разрешение на уведомления
+    // и подписываем его
+    $('#subscribe').on('click', function () {
+        subscribe();
+    });
+}
+
+function subscribe() {
+    // запрашиваем разрешение на получение уведомлений
+    messaging.requestPermission()
+        .then(function () {
+            // получаем ID устройства
+            messaging.getToken()
+                .then(function (currentToken) {
+                    console.log(currentToken);
+
+                    // if (currentToken) {
+                    //     sendTokenToServer(currentToken);
+                    // } else {
+                    //     console.warn('Не удалось получить токен.');
+                    //
+                    // }
+                })
+                .catch(function (err) {
+                    console.warn('При получении токена произошла ошибка.', err);
+                });
+    })
+    .catch(function (err) {
+        console.warn('Не удалось получить разрешение на показ уведомлений.', err);
+    });
+}
