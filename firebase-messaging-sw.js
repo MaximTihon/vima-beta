c4fea17682e8323657815380b3ab9ec8dadd5492
max-Lenova <max21250@mail.ru>
2019-06-09 21:31:12 +0300

    dasdas

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,8 +9,7 @@ const messaging = firebase.messaging();
 // браузер поддерживает уведомления
 // вообще, эту проверку должна делать библиотека Firebase, но она этого не делает
 if ('Notification' in window) {
-    var messaging = firebase.messaging();
-
+    
     // пользователь уже разрешил получение уведомлений
     // подписываем на уведомления если ещё не подписали
     if (Notification.permission === 'granted') {
